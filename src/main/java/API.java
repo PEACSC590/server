71f64e44612400cd6baa3ad1370e59f77c9c541b
jczimm <jczimm.alias@gmail.com>
2016-10-09 15:32:10 -0400

    started an example that actually uses the db (cannot connect yet)

--- src/main/java/API.java
+++ src/main/java/API.java
@@ -1,20 +1,35 @@
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 
 import spark.Request;
 
 public class API {
 
 	private DB db;
+	private DBCollection usersCollection;
+	private DBCollection itemsCollection;
 
 	public API(DB db) {
 		this.db = db;
+
+		this.usersCollection = this.db.getCollection("users");
+		this.itemsCollection = this.db.getCollection("items");
 	}
 
-	public Set<String> getItems() {
-		return db.getCollectionNames();
+	public List<String> getItems() {
+		List<String> itemStrings = new LinkedList<String>();
+
+		List<DBObject> items = itemsCollection.find().toArray();
+		for (DBObject item : items)
+			itemStrings.add(item.toMap().toString());
+
+		return itemStrings;
 	}
 
 	public Map<String, String> getBody(Request request) {
