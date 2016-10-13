b09e43d8fa0940bd1eaf4756d97bbe9b052a07b9
jczimm <jczimm.alias@gmail.com>
2016-10-13 02:53:50 -0400

    added query functionality to /list-items tool

--- src/main/java/API.java
+++ src/main/java/API.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
@@ -23,10 +24,10 @@ public class API {
 		this.itemsCollection = this.db.getCollection("items");
 	}
 
-	public List<String> getItems() {
+	public List<String> getItems(Bson query) {
 		List<String> itemStrings = new LinkedList<String>();
 
-		FindIterable<Document> items = itemsCollection.find();
+		FindIterable<Document> items = itemsCollection.find(query);
 		for (Document item : items)
 			itemStrings.add(item.toJson());
 
