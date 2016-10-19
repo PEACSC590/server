e6ef4cd4b5ea18d8b17d41a0bc7008fd1f716f16
Alec Sun <sundogx@gmail.com>
2016-10-19 16:55:11 -0400

    testing login

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -3,34 +3,52 @@ import java.net.*;
 
 public class Login {
 
-    static final String kuser = "asun"; // your account name
-    static final String kpass = "moomoo"; // retrieve password for your account 
+    // username and pw
+    static String username = ""; // your account name
+    static String password = ""; // retrieve password for your account 
 
     static class MyAuthenticator extends Authenticator {
         public PasswordAuthentication getPasswordAuthentication() {
-            // I haven't checked getRequestingScheme() here, since for NTLM
-            // and Negotiate, the usrname and password are all the same.
             System.err.println("Feeding username and password for " + getRequestingScheme());
-            return (new PasswordAuthentication(kuser, kpass.toCharArray()));
+            return (new PasswordAuthentication(username, password.toCharArray()));
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static boolean login(String user, String pass) throws Exception {
+        username = user;
+        password = pass;
+        // URL of exeter website
         String temp_url = "https://www.outlook.com/owa/exeter.edu";
         URL obj = new URL(temp_url);
+
         HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
         connection.setRequestMethod("GET");
+
+        // make sure this returns 401 so that website has moved
         System.out.println(connection.getResponseCode());
 
         // Redirected e-mail URL
+        // moo
         URL url = connection.getURL();
 
         Authenticator.setDefault(new MyAuthenticator());
-        System.out.println("moo");
-        InputStream ins = url.openConnection().getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(ins));
-        String str;
-        while((str = reader.readLine()) != null)
-            System.out.println(str);
+        //System.out.println("moo");
+        try {
+            InputStream ins = url.openConnection().getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(ins));
+            String str;
+            while((str = reader.readLine()) != null) {
+                System.out.println(str);
+            }
+            
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws Exception {
+        
+        
     }
 }
