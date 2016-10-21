c832d234c7ffbe5a242b54a2356e04f110f58727
sundogx <sundogx@gmail.com>
2016-10-21 16:16:35 -0400

    Delete TestLogin.java

--- src/test/java/TestLogin.java
+++
@@ -1,9 +0,0 @@
-
-public class TestLogin {
-
-    public static void main(String[] args) {
-        boolean good = false;
-        good = Login.login("asun1", "moo");
-        System.out.println(good);
-    }
-}
