b56f2cf501778a7b9d3d779c45c983e25736c6b3
Jacob Zimmerman <jczimm@jczimm.com>
2016-10-26 19:08:44 -0400

    UtilTest.java: finish queryStringToHashMap test and improve the method itself

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -3,12 +3,24 @@ import java.util.HashMap;
 public class Util {
 
 	public static HashMap<String, String> queryStringToHashMap(String qs) {
-		HashMap<String, String> map = new HashMap<String, String>();
+		HashMap<String, String> map = new HashMap<>();
 
 		String[] attributes = qs.split("&"), parts;
-		for (String attribute : attributes) {
-			parts = attribute.split("=");
-			map.put(parts[0], parts[1]);
+		String val;
+		if (!attributes[0].equals("")) {
+			for (String attribute : attributes) {
+				parts = attribute.split("=");
+
+				if (parts.length == 0 || parts[0].equals(""))
+					continue;
+
+				if (parts.length == 1)
+					val = "";
+				else
+					val = parts[1];
+
+				map.put(parts[0], val);
+			}
 		}
 
 		return map;
--- src/test/java/UtilTest.java
+++ src/test/java/UtilTest.java
@@ -4,16 +4,32 @@ import static org.junit.Assert.*;
 import java.util.HashMap;
 
 public class UtilTest {
-	
+
 	@Test
-	public void testQueryStringToHashMap() {
-		
-		String qs = "val1=1&foo=32";
+	public void queryStringToHashMap() {
+
+		String qs;
 		HashMap<String, String> map = new HashMap<>();
+
+		// test valid input
+		qs = "val1=1&foo=32";
+		// map.clear();
 		map.put("val1", "1");
 		map.put("foo", "32");
 		assertEquals(Util.queryStringToHashMap(qs), map);
-		
+
+		// test blank input
+		qs = "";
+		map.clear();
+		assertEquals(Util.queryStringToHashMap(qs), map);
+
+		// test input containing empty values and empty keys
+		qs = "val1=&foo=32&=empty";
+		map.clear();
+		map.put("val1", "");
+		map.put("foo", "32");
+		assertEquals(Util.queryStringToHashMap(qs), map);
+
 	}
 
 }
