8c39e07e16b658da0077772a3635c1edbe99378e
Jacob Zimmerman <jczimm@jczimm.com>
2016-11-13 20:57:51 -0500

    Util methods to convert docs into maps

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,5 +1,9 @@
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
+
+import org.bson.Document;
 
 public class Util {
 
@@ -26,9 +30,34 @@ public class Util {
 
 		return map;
 	}
-	
+
 	public static String generateUUID() {
 		return UUID.randomUUID().toString();
+	}
+
+	public static HashMap<String, Object> convertDocumentToMap(Document doc) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+
+		String[] keys = doc.keySet().toArray(new String[0]);
+		Object val;
+		for (int i = 0; i < keys.length; i++) {
+			val = doc.get(keys[i]);
+			if (keys[i].getClass().toString() == "Document")
+				val = convertDocumentToMap((Document) val);
+			map.put(keys[i], val);
+		}
+
+		return map;
+	}
+
+	public static List<HashMap<String, Object>> convertListOfDocsIntoListOfMaps(List<Document> docs) {
+		List<HashMap<String, Object>> list = new LinkedList<HashMap<String, Object>>();
+
+		for (int i = 0; i < docs.size(); i++) {
+			list.add(convertDocumentToMap(docs.get(i)));
+		}
+
+		return list;
 	}
 
 }
